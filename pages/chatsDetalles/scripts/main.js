092f197a48e939bb1a7eb5ad9743c18da0ea81cd
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2023-12-19 16:20:12 -0500

    TERMINACION DE CHAT DETALLES, FALTA CONECTARLO CON CHATS Y ERROR DE PUT O PATCH

---
+++ pages/chatsDetalles/scripts/main.js
@@ -0,0 +1,93 @@
+import { obtenerChat, enviarMensaje } from "../../../scripts/chatAxios/chat.js";
+import { buscarUsuarios } from "../../../scripts/usuarioAxios/usuarios.js";
+
+const idUsuario1 = "1"
+const idUsuario2 = "2"
+let idChat;
+const nombreChatHTML = document.getElementById("nombreChat")
+const imagenChatHTML = document.getElementById("imagenChat")
+const contenedor = document.getElementById("mensajes")
+const botonRegresar = document.getElementById("atrasButton")
+
+botonRegresar.addEventListener('click', function () {
+    history.back()
+})
+
+const data = async () =>{
+    try {
+        const response = await buscarUsuarios(idUsuario2)
+        return response   
+    } catch (error) {
+        console.warn(error)
+        throw error
+    }
+}
+
+const chat = async () =>{
+    try {
+        const response = await obtenerChat(idUsuario1, idUsuario2)
+        return response   
+    } catch (error) {
+        console.warn(error)
+        throw error
+    }
+}
+
+//obtener info usuario 2
+
+data().then((info) => {
+    nombreChatHTML.innerText = info.data.nombre + " " + info.data.apellidos
+    imagenChatHTML.setAttribute("src", info.data.url_foto_perfil)
+}).catch((error) => {
+    console.warn(error)
+})
+
+//obtener chat
+
+function procesarMensajes(infoChat) {
+    contenedor.innerText = ""
+    const mensajes = infoChat.mensajes.split('|');
+    mensajes.forEach((mensaje) => {
+        const [remitente, texto, hora] = mensaje.match(/(\d+):'([^']+)',(\d+:\d+)/).slice(1);
+        if (remitente == idUsuario1) {
+            contenedor.innerHTML += `        
+            <div class="mensaje mensajeSalidaContenedor">
+                <p>${hora}</p>
+                <div class="contenedorTexto mensajeSalida">
+                    <div class="texto">
+                        <p>${texto}</p>
+                    </div>
+                </div>
+            </div>`
+        }else{
+            contenedor.innerHTML += `
+            <div class="mensaje mensajeEntradaContenedor">
+                <p>${hora}</p>
+                <div class="contenedorTexto mensajeEntrada">
+                    <div class="texto">
+                        <p>${texto}</p>
+                    </div>
+                </div>
+            </div>`
+        }
+    });
+}
+
+
+chat().then((infoChat) => {
+    procesarMensajes(infoChat);
+    idChat = infoChat.id
+});
+
+document.getElementById('mensajeInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        enviarMensajeTexto()
+    }
+});
+
+function enviarMensajeTexto() {
+    const inputMensaje = document.getElementById('mensajeInput').value;
+    enviarMensaje(idChat, idUsuario1, inputMensaje)
+    document.getElementById('mensajeInput').value = '';
+}
---
+++ scripts/chatAxios/chat.js
@@ -0,0 +1,44 @@
+const url = "https://vercer-adopta.vercel.app/chats/";
+
+export const obtenerChat = async (idEntrada, idSalida) => {
+    try {
+        const response = await axios.get(url);
+        const chats = response.data;
+
+        const chatEncontrado = chats.find(chat =>
+            (chat.integrantes.miembro1_id === idEntrada && chat.integrantes.miembro2_id === idSalida) ||
+            (chat.integrantes.miembro1_id === idSalida && chat.integrantes.miembro2_id === idEntrada)
+        );
+
+        return chatEncontrado;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const enviarMensaje = async (idChat, idUsuario, mensaje) => {
+    try {
+        const response = await axios.get(url);
+        const chats = response.data;
+        const chatEncontrado = chats.find(chat => chat.id === idChat);
+
+        if (chatEncontrado) {
+            const horaActual = obtenerHoraActual();
+            const nuevoMensaje = `${idUsuario}:'${mensaje}',${horaActual}`;
+            chatEncontrado.mensajes = chatEncontrado.mensajes ? `${chatEncontrado.mensajes}|${nuevoMensaje}` : nuevoMensaje;
+            await axios.patch(`${url}${idChat}`, chatEncontrado);
+            console.log('Mensaje enviado con éxito');
+        } else {
+            console.log('Chat no encontrado');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const obtenerHoraActual = () => {
+    const now = new Date();
+    const hora = now.getHours().toString().padStart(2, '0');
+    const minutos = now.getMinutes().toString().padStart(2, '0');
+    return `${hora}:${minutos}`;
+}
